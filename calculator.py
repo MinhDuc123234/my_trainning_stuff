3d04c7f7ecd85d23d3f57390cb0f17133f6fe43b
nguyen minh duc <nghiengame3969@gmail.com>
2025-05-22 09:35:18 +0700

    Lần đầu thêm file calculator.py

---
+++ calculator.py
@@ -0,0 +1,22 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Không thể chia cho 0"
+
+
+# Test thử
+if __name__ == "__main__":
+    print("3 + 2 =", add(3, 2))
+    print("10 - 5 =", subtract(10, 5))
+    print("4 * 3 =", multiply(4, 3))
+    print("8 / 2 =", divide(8, 2))
